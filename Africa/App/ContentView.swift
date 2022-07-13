097f293becb636399668aea5d7c471a69464fe8b
Dan Mori <danielstelzer@gmail.com>
2022-07-12 22:57:59 -0300

    Added Mac Catalyst

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -13,6 +13,26 @@ struct ContentView: View {
     
     @State private var isGridViewActive: Bool = false
     
+    @State private var gridLayout: [GridItem] = [ GridItem(.flexible()) ]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
+    
     var body: some View {
         NavigationView {
             
@@ -28,9 +48,25 @@ struct ContentView: View {
                                 AnimalListItemView(animal: animal)
                             }//: LINK
                         }//: LOOP
+                        
+                        CreditsView()
+                            .modifier(CenterModifier())
+                        
                     }//: LIST
+                    
                 } else {
-                    Text("Grid view is active")
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailsView(animal: animal)) {
+                                    withAnimation(.easeIn) {
+                                        AnimalGridItemView(animal: animal)
+                                    }
+                                }//: LINK
+                            }//: LOOP
+                        }//: GRID
+                        .padding(10)
+                    }//: ScrollView
                 }//: Condition
             }//: Group
             .listStyle(.plain)
@@ -50,8 +86,11 @@ struct ContentView: View {
                         Button(action: {
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            withAnimation(.easeIn) {
+                                gridSwitch()
+                            }
                         }) {
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
                         }
